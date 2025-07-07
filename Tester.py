1607e8ba2efaf4f37e8f80203516385c4f13902a
JKJK <122061569+JohnPham69@users.noreply.github.com>
2025-07-07 21:57:18 +0700

    Update Tester.py

--- Tester.py
+++ Tester.py
@@ -338,7 +338,7 @@ with st.sidebar:
 
 with st.sidebar:
     st.page_link("Test_AI_page.py", label=_("Tutor AI")) # Page link with icon
-    st.page_link("Test_learn_page.py", label=_("Learning")) # New page link with icon
+    st.page_link("Test_learn_page.py", label=_("Learning with AI")) # New page link with icon
     st.page_link("Test_practice_page.py", label=_("Practice / Quiz")) # Page link with icon
     st.page_link("Test_leader_page.py", label=_("Leaderboard")) # New page link with icon
     
