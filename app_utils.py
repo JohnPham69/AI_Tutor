6fce4d3500111e8d75af8848e0ca10daf30ee69d
JKJK <122061569+JohnPham69@users.noreply.github.com>
2025-07-10 15:30:41 +0700

    Update app_utils.py

--- app_utils.py
+++ app_utils.py
@@ -2,10 +2,12 @@ import streamlit as st
 from streamlit_cookies_controller import CookieController
 
 def get_cookie_controller():
-    
     controller = CookieController()
     try:        
         controller.refresh()
     except Exception:
         pass
     return controller
+
+def getVal(controller, val):
+    return controller.get(val)
