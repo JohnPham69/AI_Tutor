0276d27fe9e520cc01b4d589525f4976a77b92b4
JKJK <122061569+JohnPham69@users.noreply.github.com>
2025-07-10 22:13:55 +0700

    Update Learn_page.py

--- Learn_page.py
+++ Learn_page.py
@@ -9,9 +9,10 @@ from streamlit_cookies_manager import CookieManager
 
 # This should be on top of your script
 cookies = get_cookies_manager()
-if not cookies.ready():
+while not cookies.ready():
     # Wait for the component to load and send us current cookies.
     st.stop()
+st.rerun()
 # Global variable
 follow_up = [] # an array that stores follow_up quesiotns
 
