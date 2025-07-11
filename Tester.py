d50ba06f677dde4eb1129cd2acb19a44ff746fa2
JKJK <122061569+JohnPham69@users.noreply.github.com>
2025-07-11 09:47:19 +0700

    Update Tester.py

--- Tester.py
+++ Tester.py
@@ -28,7 +28,7 @@ _ = get_translator() # Get the translator instance
 # NO_LESSON_OPTION_TEXT = _("No lesson")
 
 chat_page = st.Page("./aitutor.py", title = _("Tutor AI"))
-practice = st.Page("./Practice_page.py", title=_("Practice"))
+practice = st.Page("./practice.py", title=_("Practice"))
 leaderboard_page = st.Page("./Leader_page.py", title=_("Leaderboard")) # New page for leaderboard
 learning_page = st.Page("./Learn_page.py", title=_("Learning with AI"), default = True) # New page for learning
 
@@ -312,7 +312,7 @@ with st.sidebar:
     with st.expander(r"$\textsf{\large " + ("📜\t") + _("Study") + "}$", expanded=True):
         st.page_link("Learn_page.py", label=_("Learning with AI"), icon="🐻") # New page link with icon
         st.page_link("aitutor.py", label=_("Tutor AI"), icon="🐯") # Page link with icon
-        st.page_link("Practice_page.py", label=_("Practice"), icon="🐼") # Page link with icon
+        st.page_link("practice.py", label=_("Practice"), icon="🐼") # Page link with icon
         st.page_link("Leader_page.py", label=_("Leaderboard"), icon="🎓") # New page link with icon
 
     with st.expander(r"$\textsf{\large " + ("🔧\t") + _('Config') + "}$"): # Can change Large into Huge and footnotesize
