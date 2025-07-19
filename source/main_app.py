9e2af943a24c108d0feb6672e34f75009e4f3472
JKJK <122061569+JohnPham69@users.noreply.github.com>
2025-07-19 07:18:13 +0700

    Update main_app.py

--- source/main_app.py
+++ source/main_app.py
@@ -391,9 +391,10 @@ with st.sidebar:
         )
         
         if tone:
+            st.write(tone)
             controller.set('user_ai_tone', tone)
             st.session_state['ai_tone'] = cookies.get('user_ai_tone')
-            st.write(st.session_state.get('ai_tone'))
+            st.write(st.session_state.get('ai_tone') + "haha")
         
         # Initialize or update selected_lesson_contexts based on sb_lesson_tester
         if 'selected_lesson_contexts' not in st.session_state:
