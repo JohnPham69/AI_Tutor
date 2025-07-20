04cfb9ab0ecf56325c351c0757ab19f06d86d75d
JKJK <122061569+JohnPham69@users.noreply.github.com>
2025-07-20 09:19:02 +0700

    Update main_app.py

--- source/main_app.py
+++ source/main_app.py
@@ -425,9 +425,9 @@ with st.sidebar:
 
         col1, col2 = st.columns(2)
         with col1:
-            tone = st.checkbox(("😜/t") + _("Funny"), value=st.session_state['ai_fun'], on_change=save_ai, kwargs={'name': 'ai_fun'})
+            tone = st.checkbox(("😜\t") + _("Funny"), value=st.session_state['ai_fun'], on_change=save_ai, kwargs={'name': 'ai_fun'})
         with col2:
-            level = st.checkbox(("🕵/t") + _("Advance"), value=st.session_state['ai_hard'], on_change=save_ai, kwargs={'name': 'ai_hard'})
+            level = st.checkbox(("🕵\t") + _("Advance"), value=st.session_state['ai_hard'], on_change=save_ai, kwargs={'name': 'ai_hard'})
         
         st.page_link("learn.py", label=_("Learning with AI"), icon="🐻") # New page link with icon
         st.page_link("aitutor.py", label=_("Tutor AI"), icon="🐯") # Page link with icon
