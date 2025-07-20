f7d8327f9417bec33160a7085048330409faf155
JKJK <122061569+JohnPham69@users.noreply.github.com>
2025-07-20 10:15:21 +0700

    Update learn.py

--- source/learn.py
+++ source/learn.py
@@ -26,6 +26,11 @@ if "uploaded_file_content" not in st.session_state:
 st.session_state.uploaded_file_content = ""
 st.session_state.messages = []
 
+with st.chat_message("assistant"):
+    st.markdown(_("Shall we start?"))
+    st.session_state.messages.append({"role": "assistant", "content": _("Shall we start?")})
+
+
 # These will now render in Streamlit's main flow, below the sticky title.
 if "messages" in st.session_state:
     for msg in st.session_state.messages:
